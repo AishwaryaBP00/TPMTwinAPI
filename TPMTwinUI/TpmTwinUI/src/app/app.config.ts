eefde3ddc44e218370cfff35786aefccc810712b
Edwin Abraham Thomas <edwinabrahamthomas@outlook.com>
2025-08-01 15:36:38 +0530

    fix http client

--- TPMTwinUI/TpmTwinUI/src/app/app.config.ts
+++ TPMTwinUI/TpmTwinUI/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { ApiConfiguration } from './api/api-configuration';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(),
     { provide: ApiConfiguration, useValue: { rootUrl: 'http://localhost:5171' } }
   ]
 };
