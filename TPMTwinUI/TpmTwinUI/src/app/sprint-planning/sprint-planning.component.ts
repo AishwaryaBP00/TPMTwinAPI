38e3548a69e4567186c4d8038526ba54b3003590
pradeep <pradeepng2804@gmail.com>
2025-08-01 13:49:00 +0530

    created ui layout

---
+++ TPMTwinUI/TpmTwinUI/src/app/sprint-planning/sprint-planning.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sprint-planning',
+  templateUrl: './sprint-planning.component.html',
+  styleUrls: ['./sprint-planning.component.scss']
+})
+export class SprintPlanningComponent {}
---
+++ TPMTwinUI/TpmTwinUI/src/sprint-planning/sprint-planning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sprint-planning',
+  standalone: true,
+  imports: [],
+  templateUrl: './sprint-planning.component.html',
+  styleUrl: './sprint-planning.component.scss'
+})
+export class SprintPlanningComponent {
+
+}
